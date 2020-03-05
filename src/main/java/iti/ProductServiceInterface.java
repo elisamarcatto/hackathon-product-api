0325b5d0c415648458e220b5a8b96e672149c36a
elisamarcatto <elisamarcatto@gmail.com>
2020-03-05 01:01:16 -0300

    Novas classes

--- src/main/java/iti/ProductServiceInterface.java
+++
@@ -1,9 +0,0 @@
-package iti;
-
-import java.util.List;
-
-public interface ProductServiceInterface {
-
-    public Product addProduct(Product product);
-    public List<Product> getAllProducts();
-}
